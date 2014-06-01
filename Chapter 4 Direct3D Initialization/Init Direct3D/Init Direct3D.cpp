10a643eb33d481d6a4a054aa788b055709bf69ff
sgajaejung <sgajaejung@gmail.com>
2014-06-01 17:49:05 +0900

    chapter4

---
+++ Chapter 4 Direct3D Initialization/Init Direct3D/Init Direct3D.cpp
@@ -0,0 +1,75 @@
+//***************************************************************************************
+// Init Direct3D.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates the sample framework by initializing Direct3D, clearing 
+// the screen, and displaying frame stats.
+//
+//***************************************************************************************
+
+#include "d3dApp.h"
+ 
+class InitDirect3DApp : public D3DApp
+{
+public:
+	InitDirect3DApp(HINSTANCE hInstance);
+	~InitDirect3DApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene(); 
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+	InitDirect3DApp theApp(hInstance);
+	
+	if( !theApp.Init() )
+		return 0;
+	
+	return theApp.Run();
+}
+
+InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
+: D3DApp(hInstance) 
+{
+}
+
+InitDirect3DApp::~InitDirect3DApp()
+{
+}
+
+bool InitDirect3DApp::Init()
+{
+	if(!D3DApp::Init())
+		return false;
+
+	return true;
+}
+
+void InitDirect3DApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void InitDirect3DApp::UpdateScene(float dt)
+{
+
+}
+
+void InitDirect3DApp::DrawScene()
+{
+	assert(md3dImmediateContext);
+	assert(mSwapChain);
+
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::Blue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	HR(mSwapChain->Present(0, 0));
+}
